d95232cfa9bdc2c5771160585d38968a25b796d9
Искрен <32600231+Quant14@users.noreply.github.com>
2022-06-20 00:55:56 +0300

    Console menu

---
+++ main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+int main() {
+	char* options[6] = { "Generate maze", "Read from file", "Write to file", "Solve manually", "Solve automatically", "Quit"};
+	char input = 0;
+	unsigned int selected = 0;
+	bool run = true;
+
+	while (run) {
+		do {
+			system("cls");
+			for (int i = 0; i < 6; i++) {
+				if (i == selected)
+					printf("> ");
+				printf("%s\n", options[i]);
+			}
+			scanf("%c", &input);
+			if (input == 'e') //enter
+				break;
+			if (input == 'w' && selected > 0)
+				selected--;
+			else if (input == 's' && selected < 5)
+				selected++;
+		} while (true);
+
+		switch (selected) {
+		case 0:
+			//generate maze func
+			break;
+		case 1:
+			//read file func
+			break;
+		case 2:
+			//write to file func
+			break;
+		case 3:
+			//solve manually func
+			break;
+		case 4:
+			//solve automatically func
+			break;
+		case 5:
+			run = false;
+			break;
+		}
+	}
+
+	return 0;
+}
