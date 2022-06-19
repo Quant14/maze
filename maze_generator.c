01394d0d366c424d4a8de529ac29a59ec603ce53
tgarmenliev <101339875+tgarmenliev@users.noreply.github.com>
2022-06-19 15:20:17 +0300

    Update maze_generator.c

--- maze_generator.c
+++ maze_generator.c
@@ -23,12 +23,40 @@ void shuffle_array(int* array, int size)
 	}
 }
 
-struct cell** generate_maze(struct cell** grid, int height, int width)
+struct cell* find_neighbours(struct cell** grid, int start_x, int start_y, int height, int width)
+{
+	struct cell* res = malloc(sizeof(struct cell) * 4);
+	if (!res) return 0;
+	int index = 0;
+	if (start_x - 1 >= 0)
+	{
+		res[index] = grid[start_x - 1][start_y];
+		index++;
+	}
+	if (start_x + 1 < width)
+	{
+		res[index] = grid[start_x + 1][start_y];
+		index++;
+	}
+	if (start_y - 1 >= 0)
+	{
+		res[index] = grid[start_x][start_y - 1];
+		index++;
+	}
+	if (start_y + 1 < height)
+	{
+		res[index] = grid[start_x][start_y + 1];
+		index++;
+	}
+	return res;
+}
+
+struct cell** generate_maze(struct cell** grid, int height, int width, int start_x, int start_y)
 {
 	//struct cell** res = grid;
 	//return res;
 
-
+	struct cell* neighbours = find_neighbours(grid, start_x, start_y, height, width);
 
 	return grid;
 }
@@ -41,7 +69,7 @@ int main()
 	srand((unsigned int)time((time_t*)NULL));
 
 	//struct cell** res = generate_maze(grid, height, width);
-	generate_maze(grid, height, width);
+	generate_maze(grid, height, width, 0, 0);
 
 	return 0;
 }
