47f9568b486fbe3555e24e5ce8eac8fc85ff514e
tgarmenliev <101339875+tgarmenliev@users.noreply.github.com>
2022-06-19 13:54:27 +0300

    Update maze generator - create struct for cells

--- maze_generator.c
+++ maze_generator.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#define WIDTH 4
+#define HEIGHT 4
+
+enum directions { N = 1, E, S, W };
+
+struct cell
+{
+	int wall_direction;
+	int visited;
+	int data;
+};
+
+
 
 int main()
 {
-
+    
 	return 0;
 }
