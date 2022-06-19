c928f44c263429f73c149225b2dd08afcddea4b4
tgarmenliev <101339875+tgarmenliev@users.noreply.github.com>
2022-06-19 14:46:58 +0300

    Update maze generation
    Поради малкото време, което беше след НВО-то за 10 клас, частта за генериране на лабиринт до 19 юни т.г. ще представлява най-семплото генериране на лабиринт.

--- maze_generator.c
+++ maze_generator.c
@@ -3,9 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-#define WIDTH 4
-#define HEIGHT 4
-
 enum directions { N = 1, E, S, W };
 
 struct cell
@@ -15,10 +12,36 @@ struct cell
 	int data;
 };
 
+void shuffle_array(int* array, int size)
+{
+	for (int i = 0; i < (size - 1); i++)
+	{
+		int r = i + (rand() % (size - i));
+		int temp = array[i];
+		array[i] = array[r];
+		array[r] = temp;
+	}
+}
 
+struct cell** generate_maze(struct cell** grid, int height, int width)
+{
+	//struct cell** res = grid;
+	//return res;
+
+
+
+	return grid;
+}
 
 int main()
 {
-    
+	int width = 4, height = 4;
+	struct cell** grid = malloc(sizeof(struct cell) * width * height);
+
+	srand((unsigned int)time((time_t*)NULL));
+
+	//struct cell** res = generate_maze(grid, height, width);
+	generate_maze(grid, height, width);
+
 	return 0;
 }
