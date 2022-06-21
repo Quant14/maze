0da81ebf100a8455c4bff17d3ac3c7e5e872f399
tgarmenliev <101339875+tgarmenliev@users.noreply.github.com>
2022-06-21 18:22:31 +0300

    Update maze_generator.c

--- maze_generator.c
+++ maze_generator.c
@@ -5,23 +5,7 @@
 #include "list.h"
 #include "set.h"
 
-enum directions { N = 1, E, S, W };
 
-struct pair
-{
-	int x, y, po;
-};
-
-void shuffle_array(int* array, int size)
-{
-	for (int i = 0; i < (size - 1); i++)
-	{
-		int r = i + (rand() % (size - i));
-		int temp = array[i];
-		array[i] = array[r];
-		array[r] = temp;
-	}
-}
 
 struct list_node_t* find_neighbours(int start_x, int start_y, int height, int width)
 {
@@ -70,23 +54,6 @@ int* generate_maze(int* grid, int height, int width, int start_x, int start_y)
 		}
 	}
 
-
-	//struct cell** res = grid;
-	//return res;
-
-	//struct cell* neighbours = find_neighbours(grid, start_x, start_y, height, width);
-	//for (int i = 0; neighbours[i].data == 0; i++)
-	//???
-	// Проверявам всеки един от съседите на текущата клетка.
-	// Проверявам всеки от съседите дали е видян преди това.
-	// Когато съседът не е видян по-рано чрез рекурсия викам отново тази функция
-	// И това се повтаря докато не стигнем някакво дъно - видян съсед или границите на лабиринта
-	// Когато нямам повече съседи се връщам назад и довършвам със същите неща останалите съседи
-	// Така се гарантира, че всички клетки от лабиринта ще са видяни
-	// И когато една клетка се доближи до друга видяна клетка се поставя стена
-	// Така се образува лабиринта
-	// Това е и идеята по образуването на лабиринта
-	// Поради изключително малкото време между НВО-то и 19 юни не съм успял да довърша този алогритъм
 	return grid;
 }
 
@@ -97,12 +64,12 @@ int main()
 {
 	int width = 3, height = 3;
 	int* grid = malloc(sizeof(int) * width * height);
+	if (!grid) return 0;
 	memset(grid, 1, sizeof(int) * width * height);
 
 	srand((unsigned int)time((time_t*)NULL));
 
-	//struct cell** res = generate_maze(grid, height, width);
-	generate_maze(grid, height, width, 0, 0);
+	int* res = generate_maze(grid, height, width, 0, 0);
 
 	return 0;
 }
