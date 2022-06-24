ba2acaa5957ff5f4ac7dc1cab932a5537c34e13e
Искрен <32600231+Quant14@users.noreply.github.com>
2022-06-25 01:00:42 +0300

    Update save.c

--- save.c
+++ save.c
@@ -2,11 +2,14 @@
 
 void save_to_file(struct maze_t* maze) {
 	char filename[100];
-	filename[0] = 0;
 
-	do {
+	printf("Enter file name: ");
+	scanf("%s", filename);
+
+	while (filename[0] == 0) {
+		printf("Invalid file name. Enter valid file name: ");
 		scanf("%s", filename);
-	} while (filename[0] == 0);
+	}
 
 	write_file(filename, maze);
 }
@@ -23,7 +26,7 @@ void write_file(char* name, struct maze_t* maze) {
 	strcat(filename, ".txt");
 
 	file = fopen(filename, "w+");
-	fprintf(file, "%d %d\n", maze->height, maze->width);
+	fprintf(file, "%d %d ", maze->height, maze->width);
 
 	for (int h = 0, i = 0; h < maze->height; h++) {
 		for (int w = 0; w < maze->width; w++, i++) {
