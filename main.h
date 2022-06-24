3c98a52beb21059968eafb91bee4dbd954f469e5
Искрен <32600231+Quant14@users.noreply.github.com>
2022-06-24 17:50:17 +0300

    Adds SDL

--- main.h
+++ main.h
@@ -5,11 +5,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#define SDL_MAIN_HANDLED
+#include<SDL.h>
+//#include<SDL_ttf.h>
+#define WIND_W 1280
+#define WIND_H 720
 
 struct maze_t {
 	int* field;
 	int width;
 	int height;
 };
+
+void print_menu(unsigned int selected);
 
 #endif
