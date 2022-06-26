7786df66b8af057fd9cf8a85ae5e74d388132fe4
MontBru <80487370+MontBru@users.noreply.github.com>
2022-06-26 21:33:18 +0300

    Update priority_list.c

--- priority_list.c
+++ priority_list.c
@@ -12,15 +12,11 @@ struct priority_list* priority_init(struct priority_list* list)
 	return list;
 }
 
-struct priority_list* priority_add(struct priority_list* list, int item, int weight, int x, int y)
+struct priority_list* priority_add(struct priority_list* list, float item, int value)
 {
 	struct priority_list_node* new_item = malloc(sizeof(struct priority_list_node));
 	new_item->data.heuristic = item;
-	new_item->data.weight = weight;
-	new_item->data.value = malloc(sizeof(struct node_t));
-	new_item->data.value->x = x;
-	new_item->data.value->y = y;
-	//new_item->data.value = num;
+	new_item->data.value = value;
 	new_item->data.visited = 0;
 	if (list->list == NULL)
 	{
@@ -29,7 +25,7 @@ struct priority_list* priority_add(struct priority_list* list, int item, int wei
 		list->list = new_item;
 		return list;
 	}
-	if (list->list->data.heuristic > item || list->list->data.heuristic == -1)
+	if (item < list->list->data.heuristic || list->list->data.heuristic == -1)
 	{
 		new_item->next = list->list;
 		list->list = new_item;
@@ -39,7 +35,7 @@ struct priority_list* priority_add(struct priority_list* list, int item, int wei
 	struct priority_list_node* curr = list->list, * prev = 0;
 	for (; curr; curr = curr->next)
 	{
-		if (curr->data.heuristic > item || curr->data.heuristic == -1)
+		if (item < curr->data.heuristic || curr->data.heuristic == -1)
 			break;
 		prev = curr;
 	}
@@ -49,10 +45,10 @@ struct priority_list* priority_add(struct priority_list* list, int item, int wei
 	return list;
 }
 
-struct priority_list* remove_list(struct priority_list* list, int x, int y)
+struct priority_list* remove_list(struct priority_list* list, int value)
 {
 	struct priority_list_node* curr = list->list, * prev = 0;
-	if (curr->data.value->x == x && curr->data.value->y == y)
+	if (curr->data.value == value)
 	{
 		prev = curr->next;
 		free(curr);
@@ -62,7 +58,7 @@ struct priority_list* remove_list(struct priority_list* list, int x, int y)
 	}
 	for (; curr; curr = curr->next)
 	{
-		if (curr->data.value->x == x && curr->data.value->y == y)
+		if (curr->data.value == value)
 		{
 			prev->next = curr->next;
 			free(curr);
@@ -74,14 +70,6 @@ struct priority_list* remove_list(struct priority_list* list, int x, int y)
 	return list;
 }
 
-void print_priority_list(struct priority_list* list)
-{
-	while (list->list)
-	{
-		printf("%d ", list->list->data.heuristic);
-		list->list = list->list->next;
-	}
-}
 
 struct priority_list* pop(struct priority_list* list, struct astar_node_t* res)
 {
@@ -91,10 +79,8 @@ struct priority_list* pop(struct priority_list* list, struct astar_node_t* res)
 		return 0;
 	}
 	struct priority_list_node* next = list->list->next;
-	//res=&list->list->data;
 	res->prev = list->list->data.prev;
 	res->value = list->list->data.value;
-	res->weight = list->list->data.weight;
 	res->visited = list->list->data.visited;
 	res->heuristic = list->list->data.heuristic;
 	free(list->list);
