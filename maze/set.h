9eab574a81de09addfeb27db81ca3fad76ff404d
MontBru <80487370+MontBru@users.noreply.github.com>
2022-06-26 21:17:12 +0300

    Update set.h

--- maze/set.h
+++ maze/set.h
@@ -15,5 +15,3 @@ void add_to_set(struct set_t* set, void* value);
 int has_in_set(struct set_t* set, void* value);
 
 void remove_from_set(struct set_t* set, void* value);
-
-void print_all_in_set(struct set_t* set);
